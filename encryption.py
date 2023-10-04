da0f965f1e3d74873f2f6812d03836cdf076dc22
Leon Markovich <leonmarkovich97@gmail.com>
2023-10-04 15:17:21 +0300

    added comments

--- encryption.py
+++ encryption.py
@@ -5,39 +5,69 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 import base64
-#you can change the salt here for more security, but use the same one each time to retrieve the data
-SALT = b'some_random_salt'
+
+# you can change the salt here for more security, but use the same one each time to retrieve the data
+SALT = b"some_random_salt"
+
+
 def generate_key(password):
+    """gerate kdf 32byts kkey matiriall for generatin encryption key later-on from users main password, using sha256 algo
+    Parameter:
+    password= my pass paswords for gui
+
+    Returns:
+    key token to insert to fernet later
+    """
     kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    iterations=100000,
-    salt=SALT,
-    length=32,  # Length of the key (32 bytes for Fernet)
-    backend=default_backend()
-)
-    key = kdf.derive(password.encode('utf-8'))
+        algorithm=hashes.SHA256(),
+        iterations=100000,
+        salt=SALT,
+        length=32,  # Length of the key (32 bytes for Fernet)
+        backend=default_backend(),
+    )
+    key = kdf.derive(password.encode("utf-8"))
     return key
+
+
 def get_key(main_pass):
+    """genrate Fernet encryption key from user mypass password
+
+    Parameters:
+    main_pass= myPass password
+
+    Returns:
+    Fernets ecryption key
+    """
+
     encryption_key = generate_key(main_pass)
     return Fernet(base64.urlsafe_b64encode(encryption_key))
 
+
 def get_data():
-    with open (file="passwords/encrypted-passwords.txt",mode='rb') as data:
-        d=data.read()
+    """get encrypted data in byts"""
+    with open(file="passwords/encrypted-passwords.txt", mode="rb") as data:
+        d = data.read()
         if not d:
             None
         else:
             return d
 
+
 def set_data(enrypted_data):
-    with open(file="passwords/encrypted-passwords.txt",mode='wb') as file:
+    """write encrypted data in bytes
+
+    Parameters:
+    encrypted data in bytes"""
+    with open(file="passwords/encrypted-passwords.txt", mode="wb") as file:
         file.write(enrypted_data)
 
-def encrypt_data(main_pass,website,new_password):
-    key=get_key(main_pass)
-    data=json.loads((decrypt_data(key=main_pass).replace("'","\"")))
-    data[website]=new_password
-    data_to_encrypt = str(data).encode("utf-8")  
+
+def encrypt_data(main_pass, website, new_password):
+    """encrypt website and new password as json encrypted bytes"""
+    key = get_key(main_pass)
+    data = json.loads((decrypt_data(key=main_pass).replace("'", '"')))
+    data[website] = new_password
+    data_to_encrypt = str(data).encode("utf-8")
     encrypted_data = key.encrypt(data_to_encrypt)
     try:
         set_data(encrypted_data)
@@ -45,16 +75,22 @@ def encrypt_data(main_pass,website,new_password):
         return False
     return True
 
+
 def decrypt_data(key):
-    data=get_data()
+    """decrypts data
+
+    Parameters:
+    key=myPass password (gui's password)
+
+    Returns:
+    json in str format
+    """
+    data = get_data()
     if not data:
-        data='{}'
+        data = "{}"
     else:
         try:
-            data=get_key(key).decrypt(data).decode("utf-8")
+            data = get_key(key).decrypt(data).decode("utf-8")
         except:
             return None
     return data
-    
-
-
--- main.py
+++ main.py
@@ -4,47 +4,61 @@ import encryption
 from tkinter import scrolledtext
 from tkinter import messagebox
 import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
-    cur_website=website_entry.get()
-    cur_user=user_name_entry.get()
-    cur_password=password_entry.get()
-    cur_main_password=main_password_entry.get()
+    """adds password to txt file in json encrypted format as bytes"""
+    cur_website = website_entry.get()
+    cur_user = user_name_entry.get()
+    cur_password = password_entry.get()
+    cur_main_password = main_password_entry.get()
     if cur_website and cur_user and cur_password and cur_main_password:
-        password={'user_name':cur_user,'password':cur_password}
-        if not encryption.encrypt_data(main_pass=cur_main_password, website=cur_website,new_password=password):
+        password = {"user_name": cur_user, "password": cur_password}
+        if not encryption.encrypt_data(
+            main_pass=cur_main_password, website=cur_website, new_password=password
+        ):
             messagebox.showinfo("Error", "something went wrong with the encryption")
         messagebox.showinfo("Success", "password as added succesfully")
     else:
         messagebox.showinfo("Information", "you didnt enter all info")
-        
+
 
 def format_data_for_display(data):
+    """reforamts json to string
+
+    Parameters:
+    data- json formated data
+
+    Returns:
+    string formated credentials"""
     formatted_text = ""
     for website, credentials in data.items():
         formatted_text += f"Website name: {website}\n"
         formatted_text += f"User name: {credentials['user_name']}\n"
         formatted_text += f"Password: {credentials['password']}\n"
-        formatted_text += "\n"  
+        formatted_text += "\n"
     return formatted_text
 
+
 def show_passwords():
-    cur_main_password=main_password_entry.get()
+    """prints passwords to another window"""
+    cur_main_password = main_password_entry.get()
     if not cur_main_password:
         messagebox.showinfo("Information", "please enter myPass password.")
         return
     passwrods_window = tk.Toplevel()
     passwrods_window.title("All passwords")
     text_widget = scrolledtext.ScrolledText(passwrods_window, wrap=tk.WORD)
-    decrypted_data=encryption.decrypt_data(cur_main_password)
-    if decrypted_data==None:
+    decrypted_data = encryption.decrypt_data(cur_main_password)
+    if decrypted_data == None:
         messagebox.showinfo("Error", "something went wrong with the decryption")
         return
-    passwords_as_json=json.loads(decrypted_data.replace("'","\""))
-    formated_data=format_data_for_display(passwords_as_json)
-    text_widget.insert(tk.END,formated_data)
+    passwords_as_json = json.loads(decrypted_data.replace("'", '"'))
+    formated_data = format_data_for_display(passwords_as_json)
+    text_widget.insert(tk.END, formated_data)
     text_widget.pack(expand=True, fill="both")
 
 
@@ -61,7 +75,7 @@ canvas.grid(row=1, column=1)
 
 # labels
 main_password_label = tk.Label(text="myPass password: ", anchor="w", pady=5)
-main_password_label.grid(row=0, column=0, sticky="w",columnspan=2)
+main_password_label.grid(row=0, column=0, sticky="w", columnspan=2)
 wbsite_label = tk.Label(text="Website URL: ", anchor="w", pady=5)
 wbsite_label.grid(row=2, column=0, sticky="w")
 user_name_label = tk.Label(text="User name: ", anchor="w", pady=5)
@@ -83,10 +97,10 @@ password_entry.grid(row=4, column=1, sticky="w", pady=5)
 # buttons
 generate_button = tk.Button(text="Generate password", width=19)
 generate_button.grid(row=4, column=1, columnspan=2, sticky="e", pady=5)
-add_button = tk.Button(text="add", width=21,command=add_password)
-add_button.grid(row=5, column=1,sticky="w")
-show_button = tk.Button(text="show", width=19,command=show_passwords)
-show_button.grid(row=5, column=1,columnspan=2,sticky="e")
+add_button = tk.Button(text="add", width=21, command=add_password)
+add_button.grid(row=5, column=1, sticky="w")
+show_button = tk.Button(text="show", width=19, command=show_passwords)
+show_button.grid(row=5, column=1, columnspan=2, sticky="e")
 
 
 window.mainloop()
