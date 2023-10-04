f3792dd95893634d7f126b1033e796b281d989c8
Leon Markovich <leonmarkovich97@gmail.com>
2023-10-04 14:15:39 +0300

    added passwords display window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import pandas as pd
 import encryption
+from tkinter import scrolledtext
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -14,12 +16,24 @@ def add_password():
     else:
         print("you didnt enter all the information for password")
 
+def format_data_for_display(data):
+    formatted_text = ""
+    for website, credentials in data.items():
+        formatted_text += f"Website name: {website}\n"
+        formatted_text += f"User name: {credentials['user_name']}\n"
+        formatted_text += f"Password: {credentials['password']}\n"
+        formatted_text += "\n"  
+    return formatted_text
+
 def show_passwords():
-    cur_website=website_entry.get()
-    if cur_website:
-        print(encryption.decrypt_data())
-    else:
-        print("you didnt enter the website name")
+    passwrods_window = tk.Toplevel()
+    passwrods_window.title("All passwords")
+    text_widget = scrolledtext.ScrolledText(passwrods_window, wrap=tk.WORD)
+    passwords_as_json=json.loads(encryption.decrypt_data().replace("'","\""))
+    formated_data=format_data_for_display(passwords_as_json)
+    text_widget.insert(tk.END,formated_data)
+    text_widget.pack(expand=True, fill="both")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -56,5 +70,6 @@ add_button = tk.Button(text="add", width=21,command=add_password)
 add_button.grid(row=5, column=1,sticky="w")
 show_button = tk.Button(text="show", width=19,command=show_passwords)
 show_button.grid(row=5, column=1,columnspan=2,sticky="e")
+
 
 window.mainloop()
